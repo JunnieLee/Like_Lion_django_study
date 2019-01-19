4e605ed3373ea8656784a6fd26e70c6841d46942
heunghari@naver.com <heunghari@naver.com>
2019-01-19 11:00:07 +0900

    updated for 2nd seminar

--- feeds/views.py
+++ feeds/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Feed  # 추가. (참고: .models == feeds.models)
+from django.shortcuts import redirect
 
 def index(request):
-    feeds = Feed.objects.all()
-    return render(request, 'feeds/index.html', {'feeds': feeds})
+    if request.method == 'GET': # 목록 조회
+        feeds = Feed.objects.all()
+        return render(request, 'feeds/index.html', {'feeds': feeds})
+    elif request.method == 'POST': # create
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
